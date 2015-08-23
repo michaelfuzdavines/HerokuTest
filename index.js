2e3549ac6649a681dc54437c1bde55795fab1f0b
Michael Uzdavines <mike@uwattorneys.com>
2015-08-23 14:48:30 -0400

    4

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
-var http = require('http');
+var WebSocketServer = require("ws").Server
+var http = require("http")
+var express = require("express")
+var app = express()
+var port = process.env.PORT || 5000
 
-http.createServer(function (request, response) {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello World\n');
-}).listen(8124);
+app.use(express.static(__dirname + "/"))
 
-console.log('Server running at http://127.0.0.1:8124/');
+var server = http.createServer(app)
+server.listen(port)
+
+console.log("http server listening on %d", port)
+
+var wss = new WebSocketServer({server: server})
+console.log("websocket server created")
+
+wss.on("connection", function(ws) {
+  var id = setInterval(function() {
+    ws.send(JSON.stringify(new Date()), function() {  })
+  }, 1000)
+
+  console.log("websocket connection open")
+
+  ws.on("close", function() {
+    console.log("websocket connection close")
+    clearInterval(id)
+  })
+})
